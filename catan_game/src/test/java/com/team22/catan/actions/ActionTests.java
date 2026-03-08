a1c1f6f74471d18455d77a7e512daa565ba6ac4a
Jack Wyand <wyandj@mcmaster.ca>
2026-03-08 11:52:12 -0400

    Add new BuildSettlement test cases to action test suite

--- catan_game/src/test/java/com/team22/catan/actions/ActionTests.java
+++ catan_game/src/test/java/com/team22/catan/actions/ActionTests.java
@@ -7,7 +7,8 @@ import org.junit.runners.Suite.SuiteClasses;
 @RunWith(Suite.class)
 @SuiteClasses({
   BuildCityTest.class,
-  BuildRoadTest.class
+  BuildRoadTest.class,
+  BuildSettlementTest.class
 })
 public class ActionTests {
   
